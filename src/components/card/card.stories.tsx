dc5eaa8865604b65f318135250c03efe1479c610
Stas-Lob4 <staslob4uck@gmail.com>
2024-09-19 11:03:33 +0200

    fix: fixed card.stories

--- src/components/card/card.stories.tsx
+++ src/components/card/card.stories.tsx
@@ -1,21 +1,20 @@
 import { ElementRef, ForwardedRef, ReactElement, ReactNode } from 'react'
 
-import { Meta, StoryObj } from '@storybook/react'
+import { Decorator, Meta, StoryContext, StoryFn, StoryObj } from '@storybook/react'
 
 import { Replace } from '../../types/helpers'
 import { Button } from '../button'
 import { Typography } from '../typography'
 import { Card, CardContentProps } from './card'
 
-type DecoratorFunction = Exclude<Meta['decorators'], undefined>[number]
-export const commonDecorator: DecoratorFunction = Story => (
+export const commonDecorator: Decorator = (Story: StoryFn, context: StoryContext) => (
   <>
     <style>{`
-          .card-content { width: 400px; max-width: 100%; }
-          .buttons-group { display: flex; gap: 16px; }
-          .button { flex: 1 1 0; }
-        `}</style>
-    <Story />
+      .card-content { width: 400px; max-width: 100%; }
+      .buttons-group { display: flex; gap: 16px; }
+      .button { flex: 1 1 0; }
+    `}</style>
+    <Story {...context} />
   </>
 )
 
@@ -24,94 +23,66 @@ type CustomRenderProps =
       children?: undefined
       renderChildren: (props: Pick<CardContentProps, 'ignoreHeader'>) => ReactNode
     })
-  | Replace<CardContentProps, { children: Iterable<ReactElement> | ReactElement }>
+  | Replace<CardContentProps, { children: ReactElement | ReactElement[] }>
 
 const CustomRender = (props: CustomRenderProps) => {
-    if (props.children === undefined) {
-      const { renderChildren, ...restProps } = props
+  if (!props.children) {
+    const { renderChildren, ...restProps } = props
 
-      return <Card {...restProps}>{renderChildren({ ignoreHeader: props.ignoreHeader })}</Card>
-    }
-
-    return <Card {...props} />
-  },
-  /**
-   * Cards are surfaces that display content and actions on a single topic.
-   * The `Card` component includes several complementary utility components to handle various use cases:
-   * * `Card.Content`: the wrapper for the Card content.
-   * * `Card.Header`: an optional wrapper for the `Card.Content`.
-   *
-   * The `Card.Content` component provides convenient paddings for its content.
-   * It also has a bottom border if it is not the last among neighboring elements.
-   * You can pass the `ignoreHerder` prop to not increase the `padding-top`
-   * of `Card.Content` immediately following `Card.Header`.
-   *
-   * The `Card.Header` component extends the `Card.Content` styles.
-   * It also aligns the content vertically and has a bottom margin if it is not the last among its neighbors
-   * (similar to the `Content` component)
-   *
-   * Components `Header` and `Content` are also available as named imports `CardHeader`, `CardContent` for convenience.
-   *
-   * Each component of `Card`-like type extends the html tag `div` with all retained props.
-   */
-  meta: Meta<CustomRenderProps & { ref: ForwardedRef<ElementRef<'div'>> }> = {
-    argTypes: {
-      children: {
-        control: false,
-        description: 'Any arbitrary `ReactNode` component',
-        table: { type: { summary: 'ReactNode' } },
-      },
-      className: {
-        description: 'A class name provided to the underlying `div` element',
-        table: { type: { summary: 'string' } },
-      },
-      ignoreHeader: {
-        control: { type: 'boolean' },
-        defaultValue: false,
-        description: `STORYBOOK_SPECIFIC_SETTING: Can be passed to \`Card.Content\` to avoid
-        adding an increased \`padding-top\` to it if the preceding child component is \`Card.
-        Header\`. By default, the indentation is increased.`,
-        table: { type: { summary: 'boolean' } },
-      },
-      ref: {
-        control: false,
-        description: 'The `ref` is forwarded to the root `div` element',
-        table: { type: { summary: 'ForwardedRef<HTMLDivElement>' } },
-      },
-    },
-    decorators: [commonDecorator],
-    excludeStories: ['cardHeader', 'cardContent', 'commonDecorator'],
-    render: CustomRender,
-    tags: ['autodocs'],
-    title: 'UI/Card',
-    // manually add the `ref` type to the `CustomRenderProps` to add its description to the `meta` object.
+    return <Card {...restProps}>{renderChildren({ ignoreHeader: props.ignoreHeader })}</Card>
   }
+
+  return <Card {...props} />
+}
+
+const meta: Meta<CustomRenderProps & { ref: ForwardedRef<ElementRef<'div'>> }> = {
+  argTypes: {
+    children: {
+      control: false,
+      description: 'Any ReactNode component',
+      table: { type: { summary: 'ReactNode' } },
+    },
+    className: {
+      description: 'Class for the root `div` element',
+      table: { type: { summary: 'string' } },
+    },
+    ignoreHeader: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+      description: 'Prevents increased top padding for Card.Content if following a Card.Header',
+      table: { type: { summary: 'boolean' } },
+    },
+    ref: {
+      control: false,
+      description: 'Forwarded ref for the root `div` element',
+      table: { type: { summary: 'ForwardedRef<HTMLDivElement>' } },
+    },
+  },
+  decorators: [commonDecorator],
+  excludeStories: ['cardHeader', 'cardContent', 'commonDecorator'],
+  render: CustomRender,
+  tags: ['autodocs'],
+  title: 'UI/Card',
+}
 
 export default meta
 type Story = StoryObj<CustomRenderProps>
 
 export const cardHeader = (
-  <>
-    <Typography.H1 component={'h2'} style={{ marginLeft: 16 }}>
-      Long UserName
-    </Typography.H1>
-  </>
+  <Typography.H1 component={'h2'} style={{ marginLeft: 16 }}>
+    Long UserName
+  </Typography.H1>
 )
+
 export const cardContent = [
   <Typography.Regular16 component={'p'} key={'short'}>
     Lorem, ipsum dolor.
   </Typography.Regular16>,
-
   <Typography.Regular16 component={'p'} key={'long'}>
-    {/* cSpell: disable */}
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in, mollitia facilis nisi
     praesentium necessitatibus. Harum veritatis odit ullam ut earum recusandae totam quia impedit
-    consequatur, illo minus, quidem in repellat possimus! Facilis obcaecati maiores tenetur! Velit,
-    tempora aperiam obcaecati ut, labore impedit, recusandae necessitatibus quas porro dolor aut
-    possimus ad perspiciatis nostrum dolores amet ullam. Optio reiciendis ab ipsum.
-    {/* cSpell: enable */}
+    consequatur, illo minus, quidem in repellat possimus! Facilis obcaecati maiores tenetur!
   </Typography.Regular16>,
-
   <div className={'buttons-group'} key={'buttons'}>
     <Button className={'button'}>Confirm</Button>
     <Button className={'button'} variant={'tertiary'}>
