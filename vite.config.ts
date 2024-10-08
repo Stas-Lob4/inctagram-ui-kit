849241e8ebf22e9f34ba93691d78d2ddc1be5bfb
Stas-Lob4 <staslob4uck@gmail.com>
2024-10-08 14:41:00 +0200

    publish v.0.1.2

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: [
+        resolve(__dirname, 'src/index.ts'),
+        resolve(__dirname, 'src/components/index.ts'),
+        resolve(__dirname, 'src/assets/icons/index.ts'),
+      ],
       // the proper extensions will be added
       fileName: 'index',
       formats: ['es'],
